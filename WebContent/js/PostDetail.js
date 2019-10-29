99deb50736931edae4adc5754e816e27f11131ef
soft02 <soft02@njwangbo-desktop>
2019-10-29 20:01:22 +0800

    2019-10-29 20:01 commit

--- WebContent/js/PostDetail.js
+++ WebContent/js/PostDetail.js
@@ -1,3 +1,8 @@
+	var E = window.wangEditor;
+	var editor = new E('#post_editor');
+	editor.customConfig.uploadImgShowBase64 = true;   // 使用 base64 保存图片
+	editor.create();
+	
 $(function(){
 	$(".post_likeNum").click(function(){
 		var href = $(this).find("use").attr("xlink:href");
@@ -7,5 +12,16 @@ $(function(){
 		else {
 			$(this).find("use").attr("xlink:href","#icon_nogood");
 		}
+	});
+	
+	$(".post_replyNum").click(function(){
+		var name = $(this).parent().parent().siblings(".reply_userInfo").find(".post_userName").text().trim();
+		var content = $(this).parent().parent().siblings(".post_content").html();
+		editor.txt.clear();
+		editor.txt.html("<blockquote contenteditable='false'>" +
+				"@<a id ='reply_userName' href='#'>" + name + "</a>:" +
+				"<blockquote>" + content + "</blockquote>" +
+			"</blockquote>");
+		editor.txt.append("<p>&nbsp;</p>");
 	});
 });
